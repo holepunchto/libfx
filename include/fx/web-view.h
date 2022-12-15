dda1bb0c935d7ca6fe05a6e9dccc10e4af25efd5
Kasper Isager Dalsgarð <kasperisager@hey.com>
2022-12-15 14:59:37 +0100

    Define WebView bridge API

--- include/fx/web-view.h
+++ include/fx/web-view.h
@@ -1,12 +1,20 @@
 #ifndef FX_WEB_VIEW_H
 #define FX_WEB_VIEW_H
 
+#include <json.h>
+
 #include "../fx.h"
 
 typedef struct fx_web_view_s fx_web_view_t;
 
+typedef void (*fx_web_view_reply_cb)(json_t message);
+typedef void (*fx_web_view_message_cb)(fx_web_view_t *web_view, json_t message, fx_web_view_reply_cb reply);
+
 int
 fx_web_view_init (fx_t *app, double x, double y, double width, double height, fx_web_view_t **result);
+
+int
+fx_web_view_on_message (fx_web_view_t *web_view, fx_web_view_message_cb cb);
 
 int
 fx_web_view_load_url (fx_web_view_t *web_view, const char *url, size_t len);
