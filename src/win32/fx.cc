128ecdef56408bcbd803acc45b56302495906ba2
Kasper Isager <kasperisager@hey.com>
2023-03-30 11:00:13 +0200

    Implement message dispatching for `win32`

--- src/win32/fx.cc
+++ src/win32/fx.cc
@@ -1,6 +1,22 @@
+#include <assert.h>
+
 #include "../../include/fx.h"
 #include "../shared/fx.h"
 #include "fx.h"
+
+enum {
+  fx_msg_dispatch = WM_APP + 1,
+} fx_msg_t;
+
+static DWORD fx_main_thread_id = 0;
+
+static inline void
+on_dispatch (MSG msg) {
+  fx_dispatch_cb cb = (fx_dispatch_cb) msg.wParam;
+  void *data = (void *) msg.lParam;
+
+  cb(fx_main_app, data);
+}
 
 extern "C" int
 fx_platform_init (fx_t *app, fx_platform_t **result) {
@@ -47,9 +63,11 @@ fx_on_platform_resume (fx_platform_t *platform, fx_resume_cb cb) {
 
 extern "C" int
 fx_platform_run (fx_platform_t *platform) {
-  HRESULT res = CoInitializeEx(NULL, COINIT_APARTMENTTHREADED);
+  HRESULT res = CoInitialize(NULL);
 
   if (FAILED(res)) return -1;
+
+  fx_main_thread_id = GetCurrentThreadId();
 
   if (platform->on_launch) platform->on_launch(fx_main_app);
 
@@ -59,9 +77,15 @@ fx_platform_run (fx_platform_t *platform) {
   while (success = GetMessage(&msg, NULL, 0, 0), success != 0) {
     if (success == -1) {
       // TODO: Figure out error handling.
-    } else {
+    } else if (msg.hwnd) {
       TranslateMessage(&msg);
       DispatchMessage(&msg);
+    } else {
+      switch (msg.message) {
+      case fx_msg_dispatch:
+        on_dispatch(msg);
+        break;
+      }
     }
   }
 
@@ -72,10 +96,16 @@ extern "C" int
 fx_platform_terminate (fx_platform_t *platform) {
   PostQuitMessage(0);
 
+  CoUninitialize();
+
   return 0;
 }
 
 extern "C" int
 fx_dispatch (fx_dispatch_cb cb, void *data) {
-  return 0;
+  assert(fx_main_thread_id);
+
+  BOOL success = PostThreadMessage(fx_main_thread_id, fx_msg_dispatch, (WPARAM) cb, (LPARAM) data);
+
+  return success ? 0 : -1;
 }
