e4f6686df50c737bfdef582bbb3d3c39b6a1153c
Parallels <kasperisager@hey.com>
2024-01-11 09:30:37 +0100

    Implement GTK dispatching and thread support

--- src/linux/fx.c
+++ src/linux/fx.c
@@ -5,6 +5,13 @@
 #include "../shared/fx.h"
 #include "fx.h"
 
+typedef struct fx_dispatch_s fx_dispatch_t;
+
+struct fx_dispatch_s {
+  fx_dispatch_cb cb;
+  void *data;
+};
+
 static void
 on_activate (GtkApplication *gtk, gpointer data) {
   fx_t *app = fx_main_app;
@@ -16,9 +23,15 @@ int
 fx_platform_init (fx_t *app, fx_platform_t **result) {
   fx_platform_t *platform = malloc(sizeof(fx_platform_t));
 
-  platform->app = gtk_application_new(NULL, 0);
+  if (fx_is_main(app)) {
+    platform->app = gtk_application_new(NULL, 0);
 
-  g_signal_connect(platform->app, "activate", G_CALLBACK(on_activate), NULL);
+    g_signal_connect(platform->app, "activate", G_CALLBACK(on_activate), NULL);
+  } else {
+    platform->app = fx_main_app->platform->app;
+
+    g_object_ref(platform->app);
+  }
 
   platform->on_launch = NULL;
   platform->on_terminate = NULL;
@@ -70,10 +83,27 @@ fx_platform_run (fx_platform_t *platform) {
 
 int
 fx_platform_terminate (fx_platform_t *platform) {
+  g_application_quit(G_APPLICATION(platform->app));
+
+  return 0;
+}
+
+static int
+on_dispatch (void *data) {
+  fx_dispatch_t *dispatch = (fx_dispatch_t *) data;
+
+  dispatch->cb(fx_main_app, dispatch->data);
+
+  free(dispatch);
+
   return 0;
 }
 
 int
 fx_dispatch (fx_dispatch_cb cb, void *data) {
+  fx_dispatch_t *dispatch = malloc(sizeof(fx_dispatch_t));
+
+  g_idle_add(on_dispatch, dispatch);
+
   return 0;
 }
