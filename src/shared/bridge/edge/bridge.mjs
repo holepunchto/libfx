cedc0d00bdf16d645376fd3d7cfa257c1f1e1d09
Kasper Isager Dalsgarð <kasperisager@hey.com>
2025-10-14 10:59:39 +0200

    Recompile bridges

--- src/shared/bridge/edge/bridge.mjs
+++ src/shared/bridge/edge/bridge.mjs
@@ -1,18 +1,16 @@
-/* global chrome */
-
 import { Bridge } from '../bridge.mjs'
 
 class EdgeBridge extends Bridge {
   constructor() {
     super()
 
-    chrome.webview.addEventListener('message', (event) => {
+    globalThis.chrome.webview.addEventListener('message', (event) => {
       this.dispatchMessage(event.data)
     })
   }
 
   postMessage(message) {
-    chrome.webview.postMessage(JSON.stringify(message))
+    globalThis.chrome.webview.postMessage(JSON.stringify(message))
   }
 }
 
--- src/shared/bridge/webkit/bridge.mjs
+++ src/shared/bridge/webkit/bridge.mjs
@@ -1,10 +1,8 @@
-/* global webkit */
-
 import { Bridge } from '../bridge.mjs'
 
 class WebKitBridge extends Bridge {
   postMessage(message) {
-    webkit.messageHandlers.bridge.postMessage(JSON.stringify(message))
+    globalThis.webkit.messageHandlers.bridge.postMessage(JSON.stringify(message))
   }
 }
 
